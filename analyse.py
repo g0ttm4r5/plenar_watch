aaeebcf2c0c270bbbcf971cf9b718111ffe11b50
meriammalik <118772161+meriammalik@users.noreply.github.com>
2022-12-19 22:11:01 +0100

    Add files via upload
    - print in variable umgeändert

--- analyse.py
+++ analyse.py
@@ -9,58 +9,72 @@ import string
 # Daten holen
 
 from opendiscourse_page import opendiscourse as Dis
+from opendiscourse_page import open_aufbereitung as Aufb
 
-list_C = Dis.get_speeches("11000010") # Id des Politikers bei Opendiscourse
 
-# dict aus Liste holen
+def haeufigstes_wort(anzahl):
 
-dtext = list_C[0]
-
-# Redeteil aus dict holen
-
-text = dtext["speechContent"]
-
-# Satzanfaenge klein schreiben
-
-text = text.lower()
-
-# Sonderzeichen entfernen (?.() usw)
-
-text = text.translate( text.maketrans("", "", string.punctuation))
-
-# in Woerter splitten
-
-words = text.split()
-
-# Haeufigkeiten
-
-haeufigkeit = dict()
-
-for wort in words:
-    if wort not in haeufigkeit:
-        haeufigkeit[wort] = 1
-        
-    else:
-        haeufigkeit[wort] = haeufigkeit[wort] + 1
-        
-print(haeufigkeit)        
-
-# Sortieren der haeufigsten Woertern
-
-lst = list()
-for key, val in list(haeufigkeit.items()):
-    lst.append((val, key))
+    list_C = Dis.get_speeches("11000010") # Id des Politikers bei Opendiscourse
     
-lst.sort(reverse = True)
-for key, val in lst[:1]:
-    print(key, val)    
-
-# Summe aller Woerter
-total = len(text.split())
-print("Es sind " + str(total) + " Woerter.")
-
-#Laenge des Textes
-
-print(len(text))
-
-# Durchschnittliche Laenge aller Texte eines Politikers 
+    # dict aus Liste holen
+    
+    dtext = list_C[0]
+    
+    # Redeteil aus dict holen
+    
+    text = dtext["speechContent"]
+    
+    # Satzanfaenge klein schreiben
+    
+    #text = text.lower()
+    
+    # Sonderzeichen entfernen (?.() usw)
+    
+    #text = text.translate( text.maketrans("", "", string.punctuation))
+    text = Aufb.filtertext(text,1)
+    
+    # in Woerter splitten
+    
+    words = text.split()
+    
+    # Haeufigkeiten
+    
+    haeufigkeit = dict()
+    
+    for wort in words:
+        if wort not in haeufigkeit:
+            haeufigkeit[wort] = 1
+            
+        else:
+            haeufigkeit[wort] = haeufigkeit[wort] + 1
+            
+   # print(haeufigkeit)        
+    
+    # Sortieren der haeufigsten Woertern
+    
+    lst = list()
+    for key, val in list(haeufigkeit.items()):
+        lst.append((val, key))
+        
+    lst.sort(reverse = True)
+    
+    ergebnis ="Die " +str(anzahl) +" häufigsten Wörter sind: "
+    
+    for key, val in lst[:anzahl + 1]:
+        #print(key, val)    
+        ergebnis += str(key) + " mal " + str(val) +", " 
+    
+    
+    
+    # Summe aller Woerter
+    total = len(text.split())
+    ergebnis += "\n\nEs sind " + str(total) + " Woerter."
+    
+    #Laenge des Textes
+    
+    ergebnis += "\n\nDer Text hat: " + str(len(text)) + " Zeichen."
+    # Durchschnittliche Laenge aller Texte eines Politikers 
+    
+    return ergebnis
+   
+print(haeufigstes_wort(5))
