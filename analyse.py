41d387edc7e8cfb0fe40bfb998801a8c5bb2cee8
meriammalik <118772161+meriammalik@users.noreply.github.com>
2023-01-04 21:57:58 +0100

    Add files via upload
    - Die letzten fünf Reden analysiert
    - Rückgabesätze bearbeitet
    - Zeilenumbrüche entfernt

--- analyse.py
+++ analyse.py
@@ -17,11 +17,12 @@ def zerlege_Rede(ID):
     #alle Reden zu Politiker mit ID in eine Liste schreiben
     list_C = Dis.get_speeches(ID) 
     
-    list_C = list_C[:1]
+    # nur die letzten fünf Reden betrachten
+    list_C = list_C[-5:]
     
     onlytext = list()
     
-    #alle Reden aus den Dictionaries filtern
+    #die Reden aus den Dictionaries filtern
     i = 0
     for rede in list_C:
         text = list_C[i]['speechContent']
@@ -37,21 +38,16 @@ def zerlege_Rede(ID):
 
 
 def haeufigkeit_woerter(PolitikerID, anzahl):
-    reden = zerlege_Rede(PolitikerID)
-    woerter = list()
-    
-    for rede in reden:
-       woerter.append(rede.split())
-    
-    
+    reden = zerlege_Rede(PolitikerID) 
     haeufigkeit = dict()
     
-    for wort in woerter:
-        if wort not in haeufigkeit:
-            haeufigkeit[wort] = 1
-            
-        else:
-            haeufigkeit[wort] = haeufigkeit[wort] + 1
+    for rede in reden:
+        for wort in rede:
+            if wort not in haeufigkeit:
+                haeufigkeit[wort] = 1
+                
+            else:
+                haeufigkeit[wort] = haeufigkeit[wort] + 1
     
     lst = list()
     for key, val in list(haeufigkeit.items()):
@@ -59,7 +55,7 @@ def haeufigkeit_woerter(PolitikerID, anzahl):
           
     lst.sort(reverse = True)
      
-    ergebnis ="Die " +str(anzahl) +" häufigsten Wörter sind: "
+    ergebnis ="Die"  + str(anzahl) + "häufigsten Wörter in den analysierten Reden sind:"   
      
     for key, val in lst[:anzahl + 1]:
           #print(key, val)    
@@ -68,23 +64,39 @@ def haeufigkeit_woerter(PolitikerID, anzahl):
 
 # Summe aller Woerter
 def summe_aller_woerter(PolitikerID): 
-      reden = zerlege_Rede(PolitikerID)
+      #alle Reden zu Politiker mit ID in eine Liste schreiben
+      list_C = Dis.get_speeches(PolitikerID) 
+      #nur die letzten fünf Reden
+      list_C = list_C[-5:]
+      
+      reden = []
+        
+      for eintrag in list_C:
+          reden.append(eintrag['speechContent'])
+
+    
       woerter = list()
       
       for rede in reden:
-         woerter.append(rede.split())
+         woerter.extend(rede.split())
          
       total = len(woerter)
       
-      ergebnis = "\n\nEs sind " + str(total) + " Woerter."
+      ergebnis = "\n\nDie analysierten Reden haben eine Gesamtlänge von " + str(total) + " Woerter."
       
       return ergebnis
 
 #Laenge des Textes
 def durchschnitt_laenge(PolitikerID):
-      reden = zerlege_Rede(PolitikerID)
+      #alle Reden zu Politiker mit ID in eine Liste schreiben
+      list_C = Dis.get_speeches(PolitikerID) 
+      reden = []
+        
+      for eintrag in list_C:
+          reden.append(eintrag['speechContent'])
+         
       
-      laenge_aller_reden = None
+      laenge_aller_reden = 0
       zaehler = 0
       
       for rede in reden:
@@ -93,9 +105,11 @@ def durchschnitt_laenge(PolitikerID):
     
       durchschnitt = laenge_aller_reden / zaehler
     
-      ergebnis = "\n\nDie Reden haben eine durchscnittliche Länge von: " + durchschnitt + " Wörtern."
+      ergebnis = "\n\nDie analysierten Reden haben eine durchschnittliche Länge von " + str(durchschnitt.round(2)) + " Wörtern."
       return ergebnis
 
 ########Debug#######
-print(summe_aller_woerter("11000010"))
+#print(summe_aller_woerter("11000010"))
+#print(durchschnitt_laenge("11000010"))
+print(haeufigkeit_woerter("11000010",5))
  
